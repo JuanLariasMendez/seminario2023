5635e4758f8aa94ed6be057cb71b71de2426d66b
JuanLariasMendez <jlariasm@miumg.edu.gt>
2023-08-17 16:06:56 -0600

    Modificación en el index, se agregaron los botones dentro del boton anular, y se creo un archivo JS

---
+++ WebSeminario2023/web/archivos/script.js
@@ -0,0 +1,11 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//Habilita o desabilita los botones de guardar y modificar en el formulario de ingreso
+function habilita(idElemento, idElemento2){
+    document.getElementById(idElemento).style.display="block";
+    document.getElementById(idElemento2).style.display="none";
+}
